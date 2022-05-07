7e3527bd46f6d66eb6528acb2d0a43840ca5d7e7
Veedhaan <104905069+Veedhaan@users.noreply.github.com>
2022-05-07 11:27:24 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,42 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+   this.smokeImage=loadImage("sprites/smoke.png");
+   this.trejectory= [];         
+   
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+var position= [this.body.position.x,this.body,position.y];
+this.trejectory.push (position);
+ 
+for(var i=0; i<this.trajectory.length; i++)
+
+{ image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+   
+
+
+  
+}
+
+
+
+
+
+
+
+  }
+} 
+ 
+
+
+
+
+
+
